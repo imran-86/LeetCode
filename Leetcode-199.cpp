57a476a9aaa22989baca833a8aec89e5244fe1bc
student-Imran <ahmedimranmd55@gmail.com>
2026-04-10 11:25:25 +0600

    Solved

---
+++ Leetcode-199.cpp
@@ -0,0 +1,109 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+    
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(!root){
+            return ans;
+        }
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+            int size = q.size();
+            for(int i=0;i<size;i++){
+                TreeNode* front = q.front();
+                q.pop();
+                if(i==size-1){
+                    ans.push_back(front->val);
+                }
+                if(front->left){
+                    q.push(front->left);
+                }
+                if(front->right){
+                    q.push(front->right);
+                }
+            }
+        }
+        return ans;
+    }
+};
+    
+
+class TreeHelper {
+public:
+    TreeNode* buildTree(vector<int>& nodes) {
+        if (nodes.empty() || nodes[0] ==-1) return nullptr;
+        TreeNode* root = new TreeNode(nodes[0]);
+        queue<TreeNode*> q;
+        q.push(root);
+        int i = 1;
+        while (!q.empty() && i < nodes.size()) {
+            TreeNode* curr = q.front();
+            q.pop();
+            if (i < nodes.size() && nodes[i] != -1) {
+                curr->left = new TreeNode(nodes[i]);
+                q.push(curr->left);
+            }
+            i++;
+            if (i < nodes.size() && nodes[i] != -1) {
+                curr->right = new TreeNode(nodes[i]);
+                q.push(curr->right);
+            }
+            i++;
+
+        }
+        
+        return root;
+    }
+     void printTree(TreeNode* root) {
+        if(root == nullptr){
+            // cout << "null" << endl;
+            return;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        cout << "[";
+        while(!q.empty()){
+            TreeNode* node = q.front();
+            q.pop();
+            if(node){
+                cout << node->val<<" ";
+                q.push(node->left);
+                q.push(node->right);
+            } 
+        }
+        cout << "]" << endl;
+    }
+    
+    
+    
+    
+    void deleteTree(TreeNode* root) {
+        if (root == nullptr) return;
+        deleteTree(root->left);
+        deleteTree(root->right);
+        delete root;
+    }
+};
+int main() {
+    TreeHelper helper;
+    Solution solution;
+    vector<int> nodes1 = {1,2,3,4,-1,-1,-1,5};  
+    TreeNode* root1 = helper.buildTree(nodes1);
+    solution.rightSideView(root1);
+    helper.deleteTree(root1);
+    return 0;
+}
